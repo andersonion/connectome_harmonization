59aa8bf407ea90e529ac1a64f33ab1c77a1cf3e8
BJ Anderson <floydmitchell@hotmail.com>
2025-08-25 14:52:51 -0400

    Adding environment verification script, etc.

--- verify_environment.bash
+++ verify_environment.bash
@@ -7,14 +7,12 @@ Created on Mon Aug 25 14:38:34 2025
 """
 
 python - <<'PY'
-import sys, numpy, pandas, sklearn, matplotlib, patsy
-print("OK ✅")
+import sys, importlib, numpy, pandas, sklearn, matplotlib, patsy
 print("Python:", sys.version.split()[0])
 print("numpy:", numpy.__version__)
 print("pandas:", pandas.__version__)
 print("scikit-learn:", sklearn.__version__)
 print("matplotlib:", matplotlib.__version__)
-import importlib
 for m in ("neuroHarmonize","neuroCombat"):
     print(m, "import:", "OK" if importlib.util.find_spec(m) else "MISSING")
 PY
